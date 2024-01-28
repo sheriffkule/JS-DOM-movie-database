f01720709d78b36d020cec73890511209a97ff3c
sheriffkule <zeljko225@yahoo.com>
2024-01-28 20:29:04 +0100

    selecting the modal and add button

---
+++ assets/scripts/app.js
@@ -0,0 +1,7 @@
+const addMovieModal = document.getElementById('add-modal');
+// const addMovieModal = document.querySelector('#add-modal'); //alternative
+// const addMovieModal = document.body.children[1]; //alternative
+
+const startAddMovieButton = document.querySelector('header button');
+// const startAddMovieButton = document.querySelector('header').lastElementChild; //alternative
+
