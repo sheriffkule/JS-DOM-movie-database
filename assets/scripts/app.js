216bf0496226cf08552b6949ac579ef6a44bf1d9
sheriffkule <zeljko225@yahoo.com>
2024-01-28 20:37:23 +0100

    opening a modal

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,3 +5,9 @@ const addMovieModal = document.getElementById('add-modal');
 const startAddMovieButton = document.querySelector('header button');
 // const startAddMovieButton = document.querySelector('header').lastElementChild; //alternative
 
+const toggleMovieModal = () => {
+    addMovieModal.classList.toggle('visible');
+}
+
+
+startAddMovieButton.addEventListener('click', toggleMovieModal);
