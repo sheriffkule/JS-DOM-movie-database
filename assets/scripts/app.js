65852fac79e692488dc4255ce0f903530f151e36
sheriffkule <zeljko225@yahoo.com>
2024-01-28 21:05:25 +0100

    controling the backdrop

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,9 +5,28 @@ const addMovieModal = document.getElementById('add-modal');
 const startAddMovieButton = document.querySelector('header button');
 // const startAddMovieButton = document.querySelector('header').lastElementChild; //alternative
 
+const backdrop = document.getElementById('backdrop');
+// const backdrop = document.body.firstElementChild; //alternative
+
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+
+const toggleBackdrop = () => {
+    backdrop.classList.toggle('visible');
+};
+
 const toggleMovieModal = () => {
     addMovieModal.classList.toggle('visible');
-}
+    toggleBackdrop();
+};
 
+const cancelAddMovie = () => {
+    toggleMovieModal();
+};
+
+const backdropClickHandler = () => {
+    toggleMovieModal();
+};
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', toggleMovieModal);
+cancelAddMovieButton.addEventListener('click', cancelAddMovie);
