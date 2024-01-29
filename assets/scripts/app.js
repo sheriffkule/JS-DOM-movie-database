2f5bfde07daef5f9f56fd94f8d464bdc535e662d
sheriffkule <zeljko225@yahoo.com>
2024-01-29 14:40:05 +0100

    delete movie elements

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -24,7 +24,20 @@ const updateUI = () => {
     }
 };
 
-const renderNewMovieElement = (title, imageUrl, rating) => {
+const deleteMovieHandler = (movieId) => {
+    let movieIndex = 0;
+    for (const movie of movies) {
+        if (movie.id === movieId) {
+            break;
+        }
+        movieIndex++;
+    }
+    movies.splice(movieIndex, 1);
+    const listRoot = document.getElementById('movie-list');
+    listRoot.children[movieIndex].remove();
+};
+
+const renderNewMovieElement = (id, title, imageUrl, rating) => {
     const newMovieElement = document.createElement('li');
     newMovieElement.className = 'movie-element';
     newMovieElement.innerHTML = `
@@ -35,6 +48,8 @@ const renderNewMovieElement = (title, imageUrl, rating) => {
     <p>${rating}/5 stars</p>
     </div>
     `;
+
+    newMovieElement.addEventListener('click', deleteMovieHandler.bind(null, id));
     const listRoot = document.getElementById('movie-list');
     listRoot.append(newMovieElement);
 };
@@ -75,6 +90,7 @@ const addMovieHandler = () => {
         return;
     }
     const newMovie = {
+        id: Math.random().toString(),
         title: titleValue,
         image: imageUrlValue,
         rating: ratingValue
@@ -84,7 +100,12 @@ const addMovieHandler = () => {
     console.log(movies);
     toggleMovieModal();
     clearMovieInput();
-    renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating);
+    renderNewMovieElement(
+        newMovie.id,
+        newMovie.title,
+        newMovie.image,
+        newMovie.rating
+    );
     updateUI();
 };
 
